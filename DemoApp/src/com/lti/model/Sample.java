145f8539ef18cf539942a5fb9bc0fa088a9025e8
Aquib Ahmad <master.aquib786@gmail.com>
2020-12-17 17:02:30 +0530

    Show Function Added

---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,14 @@
+package com.lti.model;
+
+public class Sample {
+	
+	public void show(){
+		System.out.println("Hello World");
+	}
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+
+}
