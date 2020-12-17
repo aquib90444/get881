983b237b52a82cc5bd050f5755b97157e8fc8ed3
aquib90444 <30625714+aquib90444@users.noreply.github.com>
2020-12-17 13:44:39 +0530

    Commit 2
    Added Diff method

--- Calculator.java
+++ Calculator.java
@@ -4,8 +4,14 @@ class Calculator
   {
     return n1+n2;
   }
+  public static int diff(int n1,int n2)
+  {
+    return n1-n2;
+  }
+  
   public static void main(String args[])
   {
     System.out.println(add(10,20));
+    System.out.println(diff(30,20));
   }
 }
