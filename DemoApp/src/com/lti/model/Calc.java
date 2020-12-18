ee519a6110af6df68d820e0379262e161a90f314
Aquib Ahmad <master.aquib786@gmail.com>
2020-12-18 13:45:13 +0530

    Added Product Function

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,4 +4,13 @@ public class Calc {
 	public void add(int n1,int n2){
 		System.out.println("Sum= "+(n1+n2));
 	}
+	
+	public void sub(int n1,int n2){
+		System.out.println("Difference= "+(n1-n2));
+	}
+	
+	public void prod(int n1,int n2){
+		System.out.println("Product= "+(n1*n2));
+	}
+	
 }
